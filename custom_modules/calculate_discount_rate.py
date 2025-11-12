9b0ecbf90dcfe3ec146e173c8e3949ae07fd6351
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-11-12 19:59:20 +0100

    Dodanie modułu z funkcją testową dla DAGa

---
+++ custom_modules/calculate_discount_rate.py
@@ -0,0 +1,23 @@
+import logging
+# Używamy loggera Airflow do wyświetlania komunikatów w logach zadań
+log = logging.getLogger(__name__)
+
+def calculate_discount_rate(**kwargs):
+    """
+    Symuluje skomplikowaną logikę biznesową. 
+    Oblicza 10% zniżki na podstawie podanej kwoty.
+    """
+    
+    # 1. Pobieranie Parametru z kontekstu DAG-a (opcjonalnie)
+    # W Airflow, parametry często trafiają do kwargs['op_kwargs']
+    amount = kwargs.get('op_kwargs', {}).get('sales_amount', 1000)
+    
+    # 2. Logika Biznesowa
+    discount = amount * 0.10
+    
+    # 3. Logowanie Wyniku
+    log.info(f"Otrzymana kwota sprzedaży: {amount}")
+    log.info(f"Obliczona zniżka (10%): {discount}")
+    
+    # 4. Zwrot Wartości (Airflow automatycznie zapisze to do XCom)
+    return discount
