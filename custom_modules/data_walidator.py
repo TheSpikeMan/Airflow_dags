9b2f50d57dc4efcd007a324af3ada3ee3a43f895
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-11-17 20:02:42 +0100

    Poprawka błędów

--- custom_modules/data_walidator.py
+++ custom_modules/data_walidator.py
@@ -2,14 +2,14 @@ import logging
 
 def data_validator(**kwargs):
 
-    # Konfiguracja logownia
+    # Konfiguracja logowania
     log = logging.getLogger(__name__)
 
     # Walidacja danych
     record_count = kwargs.get('record_count', 0)
     if record_count >= 100:
         log.info(f"Walidacja OK. Liczba rekordów: {record_count}.")
-        return f"Sukces! Liczba rekordów to {record_count}"
+        return record_count
     else:
         log.error(f"BŁĄD: Liczba rekordów {record_count} jest poniżej progu minimalnego 100.")
         raise ValueError
