3da2457cc74d3fd91cfe4dec67f2af8813e9c7c7
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-11-17 20:03:09 +0100

    Uwzględnienie routera w kodzie

--- python_callable_dag_test_2/dag_testing_python_callable.py
+++ python_callable_dag_test_2/dag_testing_python_callable.py
@@ -1,8 +1,10 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import BranchPythonOperator
 from datetime import datetime
 from custom_modules.data_walidator import data_validator
+from custom_modules.data_router import data_router
 
 with DAG(
     dag_id='data_validator_pipeline',
@@ -15,13 +17,18 @@ with DAG(
         task_id='validate_data_source',
         python_callable=data_validator,
         op_kwargs={
-            'record_count':250
+            'record_count':50
         },
     )
 
+    data_router = BranchPythonOperator(
+        task_id='data_router',
+        python_callable=data_router
+    )
+
     read_validation_result = BashOperator(
-        task_id = 'read_validation_result',
-        bash_command = """
+        task_id='read_validation_result',
+        bash_command="""
             echo "-----------------------------------"
             echo "Otrzymano wynik z poprzedniego zadania (XCom):"
             echo "{{ task_instance.xcom_pull(task_ids='validate_data_source') }}"
@@ -29,4 +36,22 @@ with DAG(
             """
     )
 
-    validate_data_source >> read_validation_result
+    process_data_in_production = BashOperator(
+        task_id='process_data_in_production',
+        bash_command="""
+            echo "-----------------------------------"
+            echo "INFO: Uruchomiono produkcyjny proces ETL (>=100 rekordów)."
+            """
+    )
+
+    send_low_record_warning = BashOperator(
+        task_id='send_low_record_warning',
+        bash_command="""
+            echo "-----------------------------------"
+            echo "WARNING: Wysłano alert - za mało danych (poniżej 100)."
+            """
+    )
+
+    validate_data_source >> data_router
+    data_router >> [process_data_in_production, send_low_record_warning]
+    process_data_in_production >> read_validation_result
