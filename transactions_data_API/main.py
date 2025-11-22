0b5d069bbd00ad4e63ae619419c0d6638294be65
TheSpikeMan <grzegorz.noga.94@gmail.com>
2025-11-22 17:33:01 +0100

    Podmieniam polskie opisy na angielskie

--- transactions_data_API/main.py
+++ transactions_data_API/main.py
@@ -6,53 +6,57 @@ import random
 
 app = FastAPI()
 
-### Przygotowuję zestaw danych dla API ###
-# Definiuję zbiór danych dat transakcyjnych
+""" Preparing data for API """
+# Defining dates to generate random values
 date_beginning = pendulum.date(2025, 1, 1)
 date_end = pendulum.date(2025, 12, 31)
 
-# Zapisuję daty do listy
+# Saving to list
 current_date = date_beginning
 dates_list = [current_date.add(days=i) for i in range(0, (date_end - date_beginning).days + 1, 1)]
 
-# Losowe liczby
+# Finding random numbers
 int_numbers_list = [int(round(random.random() * 100)) for i in range(0, 10000, 1)]
 
-# Produkty:
+# Defining random products
 products_list = ['laptop', 'mobile_phone', 'charger', 'lamp', 'table', 'TV', 'mouse', 'usb_c_cable', 'keyboard']
 
-# Generator przykładowych danych transakcyjnych
+# Generating random transaction data
 transactions = [{"transaction_id": random.choice(int_numbers_list),
                  "transaction_date": random.choice(dates_list),
                  "product_id": random.choice(int_numbers_list),
                  "product_name": random.choice(products_list)}
                 for i in range(0, 1000, 1)]
 
-### API ###
+
+""" API """
+
+
 @app.get("/transactions/")
 def get_transactions(
         start_date: Optional[str] = Query(None, description="Start date in YYYY-MM-DD"),
         end_date: Optional[str] = Query(None, description="End date in YYYY-MM-DD")
 ):
-    # Parsowanie daty ze parametru (string) do formatu daty
+    # Parsing date
     try:
         start_date_parsed = pendulum.parse(start_date) if start_date else date_beginning
     except pendulum.parsing.exceptions.ParserError:
-        raise HTTPException(status_code=400, detail="start_date ma niepoprawny format, użyj YYYY-MM-DD")
+        raise HTTPException(status_code=400, detail="start_date has incorrect format, use YYYY-MM-DD")
 
     try:
         end_date_parsed = pendulum.parse(end_date) if end_date else date_end
     except pendulum.parsing.exceptions.ParserError:
-        raise HTTPException(status_code=400, detail="end_date ma niepoprawny format, użyj YYYY-MM-DD")
+        raise HTTPException(status_code=400, detail="end_date has incorrect format, use YYYY-MM-DD")
 
-    # Filtrowanie danych
+    # Filtering data
     transactions_filtered = [
         t for t in transactions
         if start_date_parsed <= t["transaction_date"] <= end_date_parsed
     ]
 
-    # Zwracanie zafiltrowanego zbioru transakcji
     return transactions_filtered
+
+
 @app.get("/")
 def root():
-    return {"Main page":1}
+    return {"Main page": 1}
